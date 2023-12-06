815453157b41180b1d451868c5f89eaa90ae124f
Gert Meulyzer <cel357@gmail.com>
2023-12-06 16:29:50 +0100

    writing the output the new github way

--- main.go
+++ main.go
@@ -6,13 +6,29 @@ import (
 	"strings"
 )
 
+/*
+- name: Save state
+run: echo "{name}={value}" >> $GITHUB_STATE
+
+- name: Set output
+run: echo "{name}={value}" >> $GITHUB_OUTPUT
+*/
+
 func main() {
 	refName := os.Getenv("INPUT_REFNAME")
+	outputFile := os.Getenv("GITHUB_OUTPUT")
 
+	f, err := os.Create(outputFile)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	version := "nil"
 	parts := strings.Split(refName, "_v")
-	if len(parts) < 2 {
-		fmt.Println(`::set-output name=myOutput::nil`)
+	if len(parts) > 1 {
+		version = parts[1]
 	}
 
-	fmt.Println(fmt.Sprintf(`::set-output name=myOutput::%s`, parts[1]))
+	fmt.Fprintf(f, `versionnr=%s\n`, version)
 }
